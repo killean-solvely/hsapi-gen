5dd6807f68a7ac49f5685300ec50263bf1e6b811
killean-solvely <killean@solvely.net>
2024-10-29 08:39:10 -0400

    No longer needed

--- internal/codegen/snippets/importsAndConstants.ts
+++
@@ -1,5 +0,0 @@
-import * as hubspot from "@hubspot/api-client";
-import {
-  AssociationSpecAssociationCategoryEnum,
-  MultiAssociatedObjectWithLabel,
-} from "@hubspot/api-client/lib/codegen/crm/associations/v4";
